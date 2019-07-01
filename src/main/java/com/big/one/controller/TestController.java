e8cccaf0b97c9865b5d25ae2f1e0e8838e9743e8
appleater <appleater7@gmail.com>
2019-07-01 19:52:40 +0900

    this is vanilla java

--- src/main/java/com/big/one/controller/TestController.java
+++ src/main/java/com/big/one/controller/TestController.java
@@ -3,7 +3,7 @@ package com.big.one.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+//@Controller
 public class TestController {
 
 	
@@ -11,6 +11,13 @@ public class TestController {
 	public String gogogogo() {
 		System.out.print("Fixed 해줘");
 		System.out.print("굿!");
+		System.err.print("굿");
 		return "home";
 	}
+	public static void main(String[] args) {
+		System.out.print("Fixed 해줘");
+		System.out.print("굿!");
+		System.err.print("굿");
+	}
+	
 }
